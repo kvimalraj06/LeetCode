17c73e29cb42234d499d5fa911ec23af20024eee
de spook <kvimalraj0611@gmail.com>
2024-06-23 14:09:05 +0530

    Solved Middle of the Linked List in leetcode

---
+++ midOfLinkedList.py
@@ -0,0 +1,10 @@
+""" 876. Middle of the Linked List """
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+        
