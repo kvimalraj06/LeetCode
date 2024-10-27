dca0bf3fda24e7d097f534d647fffa219adb6161
Quixotic <apple@Vimals-mac.local>
2024-10-27 19:01:40 +0530

    Solved - 1768. Merge Strings Alternately

---
+++ LeetCode75/Merge Strings Alternately/mergeStrings.py
@@ -0,0 +1,50 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        finalString = ""
+        startIndex = 0
+        while startIndex < len(word1) or startIndex < len(word2):
+            if startIndex < len(word1):
+                finalString += word1[startIndex]
+            if startIndex < len(word2):
+                finalString += word2[startIndex]
+            startIndex += 1
+        return finalString
+        
+""" 
+Problem:
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters. """
